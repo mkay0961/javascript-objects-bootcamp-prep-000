b8d83a1936dad831c12c40dc07c853d26bc19f1a
Matthew Kay <mkay0961@gmail.com>
2019-10-15 14:02:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,3 @@
 var playlist = {song: 'es20157'}
 
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = songTitle
-  return playlist
-}
 
-function removeFromPlaylist(playlist, artistName){
-  delete playlist[artistName]
-  return playlist
-}
